3a8773802daad8794dc69026d4e80beb75f91a17
Vance Freire <greatmetal@gmail.com>
2015-01-18 18:40:29 -0800

    First compilable release
    First version of decorator control software

--- DecorMonkey/AppDelegate.h
+++ DecorMonkey/AppDelegate.h
@@ -8,8 +8,21 @@
 
 #import <Cocoa/Cocoa.h>
 
+typedef enum _CGSDebugOptions {
+    kCGSDebugOptionNone,
+    kCGSDebugOptionNoShadows = 0x4000
+} CGSDebugOptions;
+
+extern void CGSGetDebugOptions(CGSDebugOptions *options);
+extern void CGSSetDebugOptions(CGSDebugOptions options);
+
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (readwrite, retain) IBOutlet NSMenu *menu;
+@property (readwrite, retain) IBOutlet NSStatusItem *statusItem;
+
+- (IBAction)toggleDropshadow:(id)sender;
+- (IBAction)terminate:(id)sender;
 
 @end
 
--- DecorMonkey/AppDelegate.m
+++ DecorMonkey/AppDelegate.m
@@ -8,12 +8,56 @@
 
 #import "AppDelegate.h"
 
+CGSDebugOptions options;
+
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
 @end
 
 @implementation AppDelegate
+
+- (void)awakeFromNib {
+    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    NSImage *menuIcon = [NSImage imageNamed:@"MenuIcon"];
+    NSImage *highlightIcon = [NSImage imageNamed:@"MenuIcon"];
+    
+    [highlightIcon setTemplate:YES];
+    
+    [[self statusItem] setImage:menuIcon];
+    [[self statusItem] setAlternateImage:highlightIcon];
+    [[self statusItem] setMenu:[self menu]];
+    [[self statusItem] setHighlightMode:YES];
+}
+
+- (IBAction)toggleDropshadow:(id)sender {
+    NSLog(@"toggleDropshadow:");
+    [sender setState:[sender state] == NSOffState ? NSOnState : NSOffState];
+    if ([sender state] == NSOffState) {
+        options = kCGSDebugOptionNone;
+        CGSSetDebugOptions(options);
+    }
+    if([sender state] == NSOnState) {
+        options = kCGSDebugOptionNoShadows;
+        CGSSetDebugOptions(options);
+    }
+}
+
+- (IBAction)terminate:(id)sender {
+    NSLog(@"terminate:");
+    [[NSApplication sharedApplication] terminate:nil];
+}
+
+- (BOOL)validateMenuItem:(NSMenuItem *)menuItem {
+    if([[menuItem title] isEqualToString:@"Disable Dropshadow"]) {
+        CGSGetDebugOptions(&options);
+        if(options == kCGSDebugOptionNoShadows) {
+            [menuItem setState: NSOnState];
+        }
+    }
+    return YES;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
